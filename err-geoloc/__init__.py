80e229bb8465bd81ec89f909980e57eb7e4fb95a
Romain Brucker <romain.brucker@amalto.com>
2017-10-09 10:17:21 -0500

    Extending README Adding check to see if the bot is in a chatroom (conflicts with `client`) and fail if so Removing gmplot from requirements

--- err-geoloc/__init__.py
+++ err-geoloc/__init__.py
@@ -26,36 +26,41 @@ class Geoloc(BotPlugin):
         """Define your location
         usage: !geoloc set New York
         """
-        
-        geolocator = Nominatim()
-        location = geolocator.geocode(args) #Create a location array using the data provided by the user
-        if os.path.exists(self.config['json_path']): #Check if the database exists
-            with open(self.config['json_path'], 'r') as f: #Load the json database file
-                user_db = json.load(f)
+        if hasattr(msg.to, 'topic'): #Check if the bot is in a chatroom or not
+            yield("Please contact me directly in a private conversation")
         else:
-            user_db = {} #Create the database if it doesnt
-        user_db[str(getattr(msg.frm, self.config['unique_id']))] = { #Insert the user data into an object
-                "user": str(msg.frm.person),
-                "place": location[0],
-                "latitude":location[1][0],
-                "longitude":location[1][1],
-        }
-        with open(self.config['json_path'], 'w') as f: #Write the user data to the json file
-            json.dump(user_db, f)
-        yield("Your location is set as %s" % user_db[str(getattr(msg.frm, self.config['unique_id']))]['place']) #Prompt the location back to the user to make sure he chose the right one
-        yield("Here is the current updated map : %s" % self.config['url'])
+            geolocator = Nominatim()
+            location = geolocator.geocode(args) #Create a location array using the data provided by the user
+            if os.path.exists(self.config['json_path']): #Check if the database exists
+                with open(self.config['json_path'], 'r') as f: #Load the json database file
+                    user_db = json.load(f)
+            else:
+                user_db = {} #Create the database if it doesnt
+            user_db[str(getattr(msg.frm, self.config['unique_id']))] = { #Insert the user data into an object
+                    "user": str(msg.frm.person),
+                    "place": location[0],
+                    "latitude":location[1][0],
+                    "longitude":location[1][1],
+            }
+            with open(self.config['json_path'], 'w') as f: #Write the user data to the json file
+                json.dump(user_db, f)
+            yield("Your location is set as %s" % user_db[str(getattr(msg.frm, self.config['unique_id']))]['place']) #Prompt the location back to the user to make sure he chose the right one
+            yield("Here is the current updated map : %s" % self.config['url'])
 
     @botcmd()
     def geoloc_get(self, msg, args):
         """Get your current location
         usage: !geoloc get
         """
-        with open(self.config['json_path'], 'r') as f:
-            user_db = json.load(f)
-        if user_db[str(getattr(msg.frm, self.config['unique_id']))] is None:
-            yield("Please set a location with !loc set")
-            raise SystemExit(0)
+        if hasattr(msg.to, 'topic'):
+            yield("Please contact me directly in a private conversation")
         else:
-            yield("Your location is set as %s" % user_db[str(getattr(msg.frm, self.config['unique_id']))]['place'])
+            with open(self.config['json_path'], 'r') as f:
+                user_db = json.load(f)
+            if user_db[str(getattr(msg.frm, self.config['unique_id']))] is None:
+                yield("Please set a location with !loc set")
+                raise SystemExit(0)
+            else:
+                yield("Your location is set as %s" % user_db[str(getattr(msg.frm, self.config['unique_id']))]['place'])
 
 ## File : __init__.py ends
